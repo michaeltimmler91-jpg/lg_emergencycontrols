b67e2839e0314b55fd811e36c0c50a3d16bd151b
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-08-01 19:09:15 +0200

    Initial FiveM resource manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'lg_emergencycontrols'
+author 'LG'
+description 'Getrennte Steuerung fuer Blaulicht und Martinshorn'
+version '0.1.0'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
